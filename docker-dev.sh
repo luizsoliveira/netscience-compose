1c7ec40d934a0ffe4c9bc04a406596706a9408a9
Luiz Oliveira <luizsoliveira@gmail.com>
2023-10-09 23:44:51 -0700

    Creating separate env files and separate docker-compose files for DEV and PROD environments

---
+++ docker-dev.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker-compose --env-file .env.development -f docker-compose-dev.yml down
+docker-compose --env-file .env.development -f docker-compose-dev.yml up
---
+++ docker-prod.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker-compose --env-file .env.production -f docker-compose-prod.yml down
+docker-compose --env-file .env.production -f docker-compose-prod.yml build react-admin --no-cache
+docker-compose --env-file .env.production -f docker-compose-prod.yml up
